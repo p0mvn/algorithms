137c072ea3ac82ca6884e0cde514ae83e37897ec
Roman Akhtariev <ackhtariev@gmail.com>
2018-08-16 22:19:39 -0700

    Add 876. Middle of the Linked List

---
+++ Java/MidlleLinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class MiddleLinkedList {
+    public ListNode middleNode(ListNode head) {
+        
+        return helper(head, 1, head);
+        
+    }
+    
+    private ListNode helper(ListNode cur, int count, ListNode mid) {
+        
+        
+        
+        if (cur == null) {
+            return mid;
+        }
+        
+        if(count % 2 == 0) {
+            mid = mid.next;
+        }
+        
+        return helper(cur.next, ++count, mid);
+        
+        
+        
+    }
+}
