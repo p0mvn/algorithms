ba72af7465b0fe8a1da0bf8c8b9560ca76e2b8f8
Roman Akhtariev <ackhtariev@gmail.com>
2018-08-17 18:58:25 -0700

    Add 235. Lowest Common Ancestor in BST

---
+++ Java/LCABST.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LCABST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if((q.val <= root.val && p.val >= root.val) || (p.val <= root.val && q.val >= root.val)) {
+            return root;
+        }
+        
+        if(p.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return lowestCommonAncestor(root.left, p, q);
+        
+    }  
+}
