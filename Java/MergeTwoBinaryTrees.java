80947907a3447da819d857523f3f66332e409beb
Roman Akhtariev <ackhtariev@gmail.com>
2018-08-27 17:57:27 -0700

    617. Merge Two Binary Trees

---
+++ Java/MergeTwoBinaryTrees.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        
+        if(t1 != null && t2 != null) {
+            
+            t1.val += t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+            
+        } else if (t1 != null) {
+            
+            return t1;
+            
+        } else if (t2 != null) {
+            
+            return t2;
+            
+        
+        } else {
+            
+            return null;
+        }
+        
+        return t1;
+        
+    }
+}
